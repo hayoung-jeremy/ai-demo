36c252c9ebfaf1cc5fec5e940c3be579b6e0a04f
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-06-01 15:54:54 +0900

    style : Add icons

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,9 @@ import { ImageUploader, ImageUpscaler } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-2">
-      <h1>Hi</h1>
-      <div className="border p-5 grid grid-cols-2 gap-4 min-h-[300px]">
-        <ImageUploader />
-        <ImageUpscaler />
-      </div>
-    </main>
+    <div className="p-5 grid grid-cols-2 gap-4 min-h-[300px]">
+      <ImageUploader />
+      <ImageUpscaler />
+    </div>
   );
 }
---
+++ src/components/icons/index.ts
@@ -0,0 +1,9 @@
+export { default as Logo } from "./Logo";
+export { default as ImageUpscalingOutlined } from "./ImageUpscalingOutlined";
+export { default as ImageUpscalingFilled } from "./ImageUpscalingFilled";
+export { default as KeyboardOutlined } from "./KeyboardOutlined";
+export { default as KeyboardFilled } from "./KeyboardFilled";
+export { default as VideoOutlined } from "./VideoOutlined";
+export { default as VideoFilled } from "./VideoFilled";
+export { default as SettingsOutlined } from "./SettingsOutlined";
+export { default as SettingsFilled } from "./SettingsFilled";
