1d8fee92da675b481bf22f11aa24ee532e919c9c
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-06-02 10:13:08 +0900

    chore : use dummy data for API

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "altava-b2b-clarins.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
