5efa169bb9f75fb8460ba76dcf38e91592910d80
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-06-05 16:52:27 +0900

    feat : update to use stable diffusion V4 API

--- pages/api/upscale.ts
+++ pages/api/upscale.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import { stableDiffusionV3Img2ImgURL, stableDiffusionV3base64cropURL } from "@/constants";
+import { stableDiffusionV3Img2ImgURL, stableDiffusionV3base64cropURL, v3DummyData, v4DummyData } from "@/constants";
 
 export const config = {
   api: {
@@ -10,52 +10,34 @@ export const config = {
   },
 };
 
-const data = {
-  key: process.env.SD_API_KEY,
-  prompt: "red shoes",
-  negative_prompt: null,
-  init_image: "",
-  width: "512",
-  height: "512",
-  samples: "1",
-  num_inference_steps: "30",
-  safety_checker: "no",
-  enhance_prompt: "yes",
-  guidance_scale: 7.5,
-  strength: 0.7,
-  seed: null,
-  webhook: null,
-  track_id: null,
-};
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // base64_crop api
-  // let imageLink = "";
-  // try {
-  //   const { data: resultImage } = await axios.post(stableDiffusionV3base64cropURL, {
-  //     key: process.env.SD_API_KEY,
-  //     image: req.body.init_image,
-  //     crop: "true",
-  //   });
-  //   console.log("resultImage", resultImage.link);
-  //   imageLink = resultImage.link;
-  //   const response = await axios.post(stableDiffusionV3Img2ImgURL, {
-  //     ...data,
-  //     init_image: imageLink,
-  //   });
-  //   return res.status(200).json(response.data);
-  // } catch (error) {
-  //   return res.status(500).json({ error: "Internal Server Error" });
-  // }
+  let imageLink = "";
+  try {
+    const { data: resultImage } = await axios.post(stableDiffusionV3base64cropURL, {
+      key: process.env.SD_API_KEY,
+      image: req.body.init_image,
+      crop: "true",
+    });
+    console.log("resultImage", resultImage.link);
+    imageLink = resultImage.link;
+    const response = await axios.post(stableDiffusionV3Img2ImgURL, {
+      ...v4DummyData,
+      init_image: imageLink,
+    });
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 
-  setTimeout(async () => {
-    try {
-      const data = {
-        output: ["https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg"],
-      };
-      return res.status(200).json(data);
-    } catch (error) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  }, 3000);
+  // setTimeout(async () => {
+  //   try {
+  //     const data = {
+  //       output: ["https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg"],
+  //     };
+  //     return res.status(200).json(data);
+  //   } catch (error) {
+  //     return res.status(500).json({ error: "Internal Server Error" });
+  //   }
+  // }, 3000);
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,46 @@
 export const stableDiffusionV3Img2ImgURL = "https://stablediffusionapi.com/api/v3/img2img";
 export const stableDiffusionV3base64cropURL = "https://stablediffusionapi.com/api/v3/base64_crop";
 export const stableDiffusionV4Img2ImgURL = "https://stablediffusionapi.com/api/v4/dreambooth/img2img";
+
+export const v3DummyData = {
+  key: process.env.SD_API_KEY,
+  prompt: "red shoes",
+  negative_prompt: null,
+  init_image: "",
+  width: "512",
+  height: "512",
+  samples: "1",
+  num_inference_steps: "30",
+  safety_checker: "no",
+  enhance_prompt: "yes",
+  guidance_scale: 7.5,
+  strength: 0.7,
+  seed: null,
+  webhook: null,
+  track_id: null,
+};
+export const v4DummyData = {
+  key: process.env.SD_API_KEY,
+  model_id: "midjourney",
+  prompt: "papercraft, quilling, layers, landscape",
+  negative_prompt: null,
+  init_image: "",
+  width: "512",
+  height: "512",
+  samples: "1",
+  num_inference_steps: "30",
+  safety_checker: "yes",
+  enhance_prompt: "yes",
+  guidance_scale: 7.5,
+  strength: 0.7,
+  scheduler: "UniPCMultistepScheduler",
+  seed: null,
+  lora_model: null,
+  tomesd: "yes",
+  use_karras_sigmas: "yes",
+  vae: null,
+  lora_strength: null,
+  embeddings_model: null,
+  webhook: null,
+  track_id: null,
+};
